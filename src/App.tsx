580df72e1f7da3ca646f3e9f7f4a86986ac3f3ce
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-03-04 23:37:54 +0530

    changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./components/Navbar"
+import { Github } from "./icons/Github"
 
 
 export default function App() {
@@ -7,11 +8,34 @@ export default function App() {
             <Navbar/>
             
 
-            <div className="md:text-4xl text-xl cursor-progress hover:underline animate-bounce mt-28 transition-all duration-500 flex justify-center items-center">
+            <div className="md:text-4xl font-extrabold text-purple-400 text-2xl cursor-pointer hover:underline animate-bounce mt-20 transition-all duration-500 flex justify-center items-center">
                 Launching Soon!!
             </div>
-            <div className="flex justify-center gap-5 mt-10">
+            
+
+            <div className="flex flex-col items-center justify-center mt-20">
+                <div className="text-xl underline md:text-4xl cursor-pointer font-bold">
+                    Contact the Founders & Developer:
+                </div>
+
+                <div className="mt-5 flex flex-wrap justify-center gap-10 text-center">
+                    <div onClick={() => {alert("Getting Contact details soon for Yogendra")}} className="hover:text-blue-400 hover:underline cursor-pointer text-xl font-bold">
+                        Yogendra
+                    </div>
+                    <div onClick={() => {window.open("https://shubhhere.vercel.app")}} className="hover:text-blue-400 hover:underline cursor-pointer text-xl font-bold">
+                        Shubh
+                    </div>
+                    <div onClick={() => {alert("Getting Contact details soon for Abhinav")}} className="hover:text-blue-400 hover:underline cursor-pointer text-xl font-bold">
+                        Abhinav
+                    </div>
+                    <div onClick={() => {alert("Getting Contact details soon for Ayush")}} className="hover:text-blue-400 hover:underline cursor-pointer text-xl font-bold">
+                        Ayush
+                    </div>
+                </div>
+
             </div>
+
+        
             
         </div>
     )
